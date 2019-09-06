3f6de19e6973091e4278ed1075fa06a9278dc54f
ashishzade1987 <myclassic99@gmail.com>
2019-09-06 15:21:46 +0530

    demo

---
+++ demolibrary/src/main/java/com/ngp/demolibrary/Metrics.java
@@ -0,0 +1,22 @@
+package com.ngp.demolibrary;
+
+/**
+ * Created by Ashish on 9/6/2019.
+ */
+public class Metrics {
+
+
+    String Key;
+    String Value;
+    String Unit;
+    String Label;
+
+    public Metrics(String key, String value, String unit, String label) {
+
+        this.Key = key;
+        this.Value = value;
+        this.Unit = unit;
+        this.Label = label;
+
+    }
+}
